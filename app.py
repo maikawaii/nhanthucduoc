e884cae226ceb0e58ff492a013d4de8981d6a445
maikawaii <laingocmai91@gmail.com>
2024-12-08 22:15:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,22 @@ from transformers import AutoModelForImageClassification, AutoProcessor
 import torch
 from PIL import Image
 
+# Thêm CSS để hiển thị hình nền
+forest_image_url = "https://images.unsplash.com/photo-1448375240586-882707db888b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"  # Link tới hình ảnh cánh rừng của bạn
+st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url("{forest_image_url}");
+        background-size: cover;
+        background-position: center;
+        background-attachment: fixed;
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 # Tải file labels.txt
 url = "https://raw.githubusercontent.com/maikawaii/nhanthucduoc/refs/heads/main/label.txt"
 response = requests.get(url)
