3f30159db0c33cb65601b7ec4708e19e6c512542
maikawaii <laingocmai91@gmail.com>
2024-12-08 22:24:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import torch
 from PIL import Image
 
 # Thêm CSS để hiển thị hình nền
-forest_image_url = "https://images.unsplash.com/photo-1448375240586-882707db888b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"  # Link tới hình ảnh cánh rừng của bạn
+forest_image_url = "https://plus.unsplash.com/premium_vector-1722874378083-63fa7ee2c434?q=80&w=1800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"  # Link tới hình ảnh cánh rừng của bạn
 st.markdown(
     f"""
     <style>
